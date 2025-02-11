21aaaf45178c6ca5fe17a1217edb7943b0e16144
mike <krasnoff.mike@yandex.ru>
2025-02-11 19:54:46 +0300

    HelloWorld commit

---
+++ app/build.gradle.kts
@@ -0,0 +1,26 @@
+plugins {
+    id("java")
+    id("se.patrikerdes.use-latest-versions") version "0.2.18"
+    id("com.github.ben-manes.versions") version "0.52.0"
+//    application
+}
+
+group = "hexlet.code"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+//application {
+//    mainClass.set("hexlet.code.App")
+//}
+
+dependencies {
+    testImplementation(platform("org.junit:junit-bom:5.12.0-RC1"))
+    testImplementation("org.junit.jupiter:junit-jupiter")
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
