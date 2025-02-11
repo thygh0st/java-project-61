8743459e8b312904463392956ac96c7077869e40
mike <krasnoff.mike@yandex.ru>
2025-02-11 21:31:20 +0300

    Rm Cli (should be abandoned), added games menu

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -1,8 +1,21 @@
 package hexlet.code;
 
+import java.util.Scanner;
+
 class App {
+//    private String userName;
+
     public static void main(String[] args) {
-        System.out.println("Welcome to the Brain Games!");
-        Cli.greeting();
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Please enter the game number and press Enter.");
+        System.out.println("1 - Greet\n0 - Exit");
+        System.out.print("Your choice: ");
+        int userGameChoice = sc.nextInt();
+        if (userGameChoice != 0) {
+            System.out.println("Welcome to the Brain Games!");
+            System.out.print("May I have your name? ");
+            String userName = sc.next();
+            System.out.println("Hello, " + userName + "!");
+        }
     }
 }
