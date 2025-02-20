43a149895debe49d0e6c436ff1cd89dc06840486
mike <krasnoff.mike@yandex.ru>
2025-02-20 22:02:19 +0300

    Fixes on review: cleanup builder config

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,5 +1,4 @@
 plugins {
-    id("java")
     id("se.patrikerdes.use-latest-versions") version "0.2.18"
     id("com.github.ben-manes.versions") version "0.52.0"
     application
@@ -17,10 +16,8 @@ application {
     mainClass.set("hexlet.code.App")
 }
 
-dependencies {
-    testImplementation(platform("org.junit:junit-bom:5.12.0-RC1"))
-    testImplementation("org.junit.jupiter:junit-jupiter")
-}
+//dependencies {
+//}
 
 tasks.test {
     useJUnitPlatform()
