5e7eeb13dbe88ddc5a8c5b1590040124dd4a44fe
mike <krasnoff.mike@yandex.ru>
2025-03-12 00:06:24 +0300

    added lost print

--- app/src/main/java/hexlet/code/Cli.java
+++ app/src/main/java/hexlet/code/Cli.java
@@ -4,6 +4,7 @@ import java.util.Scanner;
 public class Cli {
     public static void greeting() {
         Scanner sc = new Scanner(System.in);
+        System.out.println("Welcome to the Brain Games!");
         System.out.print("May I have your name? ");
         String userName = sc.next();
         System.out.println("Hello, " + userName + "!");
